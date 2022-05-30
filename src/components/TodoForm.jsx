0370ef0b94bf52d0a4c762b727f17fa0edf65554
Artem Davidyan <artem.davidyan@innowise-group.com>
2022-05-30 14:15:50 +0300

    TodoForm component implementation

---
+++ src/components/TodoForm.jsx
@@ -0,0 +1,64 @@
+import React, { useEffect, useRef, useState } from 'react';
+import PropTypes from 'prop-types';
+
+export default function TodoForm({ onSubmit, edit }) {
+  const [input, setInput] = useState(edit ? edit.value : '');
+
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    inputRef.current.focus();
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    onSubmit({
+      id: Math.floor(Math.random() * 10000),
+      text: input
+    });
+
+    setInput('');
+  };
+
+  const handleChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  return (
+    <form className="todo-form" onSubmit={handleSubmit}>
+      {edit ? (
+        <>
+          <input
+            type="text"
+            placeholder="Updated your item"
+            value={input}
+            name="text"
+            className="todo-form__input edit"
+            onChange={handleChange}
+            ref={inputRef}
+          />
+          <button className="todo-form__button edit">Update</button>
+        </>
+      ) : (
+        <>
+          <input
+            type="text"
+            placeholder="Add a todo"
+            value={input}
+            name="text"
+            className="todo-form__input"
+            onChange={handleChange}
+            ref={inputRef}
+          />
+          <button className="todo-form__button">Add a Todo</button>
+        </>
+      )}
+    </form>
+  );
+}
+
+TodoForm.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+  edit: PropTypes.object
+};
