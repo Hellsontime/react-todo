e7f889548aeb72fbc40ea89e31ae65314a2616dd
Artem Davidyan <artem.davidyan@innowise-group.com>
2022-05-31 16:47:52 +0300

    TodoPageContatiner implementation

---
+++ src/containers/TodoPageContainer.jsx
@@ -0,0 +1,24 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import { Navigate } from 'react-router-dom'
+import TodoList from '../components/Todo/TodoList'
+import { LOGIN_PAGE_ROUTE } from '../routes/PagesRoutes'
+import '../styles/PageStyles/TodoPage.sass'
+
+export default function TodoPageContainer() {
+  const userToken = localStorage.getItem('user-token')
+
+  // return userToken ? (
+  //   <div className="todo-app">
+  //     <TodoList />
+  //   </div>
+  // ) : (
+  //   <Navigate to={LOGIN_PAGE_ROUTE} />
+  // )
+
+  return (
+    <div className="todo-app">
+      <TodoList />
+    </div>
+  )
+}
