a36f6a1ecd61282761f3d3f449a8f8bba64dd1e8
Raman Kanapeshka <raman.kanapeshka@innowise-group.com>
2022-05-31 16:47:14 +0300

    NotAuthorizedUser Constant implementation

---
+++ src/services/constants/NotAuthorizedUser.js
@@ -0,0 +1,13 @@
+const notAuthorizedUser = {
+  user: {
+    id: NaN,
+    name: null,
+    email: null,
+    email_verified_at: null,
+    created_at: NaN,
+    updated_at: NaN
+  },
+  token: null
+}
+
+export default notAuthorizedUser
